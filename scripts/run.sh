8df5ea7a1f92455008c545529ef6d1b796399a92
qhduan <mail@qhduan.com>
2022-05-25 19:46:03 +0800

    change process of gunicorn to 2

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@
 gunicorn \
     -k uvicorn.workers.UvicornWorker  \
     --bind "0.0.0.0:8000" \
-    --workers 4 \
+    --workers 2 \
     --timeout 240 \
     --graceful-timeout 240 \
     --max-requests 0 \
